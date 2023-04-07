6b9c1bec93b97198a5966af73ec37142cf769ac1
Dimitar Nikolov <mitko.box.hs@gmail.com>
2023-04-07 09:41:53 +0300

    Add registration validations for Users and Companies

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,6 +8,14 @@ class RegistrationsController < ApplicationController
 
     def create_user
         @user = User.new(user_params)
+        if user_with_email_exists?(@user)
+            redirect_to user_sign_up_path, :flash => { :alert => 'Вече съществува орган за технически надзор с този имейл!' }
+            return
+        end
+        if user_with_company_number_exists?(@user)
+            redirect_to user_sign_up_path, :flash => { :alert => 'Вече съществува орган за технически надзор с този номер на компанията!' }
+            return
+        end
         if @user.save
             session[:user_id] = @user.id
             redirect_to root_path, :flash => { :notice => 'Успешна регистрация!' }
@@ -22,6 +30,14 @@ class RegistrationsController < ApplicationController
 
     def create_company
         @company = Company.new(company_params)
+        if company_with_email_exists?(@company)
+            redirect_to company_sign_up_path, :flash => { :alert => 'Вече съществува компания с този имейл!' }
+            return
+        end
+        if company_with_uic_exists?(@company)
+            redirect_to company_sign_up_path, :flash => { :alert => 'Вече съществува компания с този ЕИК!' }
+            return
+        end
         if @company.save
             session[:company_id] = @company.id
             redirect_to root_path, :flash => { :notice => 'Успешна регистрация!' }
@@ -38,5 +54,33 @@ class RegistrationsController < ApplicationController
 
     def company_params
         params.require(:company).permit(:email, :password, :password_confirmation, :uic, :name, :address)
+    end
+
+    def user_with_email_exists?(user)
+        if User.find_by(email: user[:email])
+            return true
+        end
+        return false
+    end
+
+    def user_with_company_number_exists?(user)
+        if User.find_by(company_number: user[:company_number])
+            return true
+        end
+        return false
+    end
+
+    def company_with_email_exists?(company)
+        if Company.find_by(email: company[:email])
+            return true
+        end
+        return false
+    end
+
+    def company_with_uic_exists?(company)
+        if Company.find_by(uic: company[:uic])
+            return true
+        end
+        return false
     end
 end
